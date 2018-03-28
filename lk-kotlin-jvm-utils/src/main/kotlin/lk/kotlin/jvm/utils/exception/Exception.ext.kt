6f1f6322eb42801d9fbd5f449fda359bff85a304
Joseph Ivie <josephivie@gmail.com>
2018-03-28 13:06:13 -0600

    Doc

--- lk-kotlin-jvm-utils/src/main/kotlin/lk/kotlin/jvm/utils/exception/Exception.ext.kt
+++ lk-kotlin-jvm-utils/src/main/kotlin/lk/kotlin/jvm/utils/exception/Exception.ext.kt
@@ -3,6 +3,9 @@ package lk.kotlin.jvm.utils.exception
 import java.io.PrintWriter
 import java.io.StringWriter
 
+/**
+ * Returns the stack trace as a string.
+ */
 fun Exception.stackTraceString():String{
     val writer = StringWriter()
     printStackTrace(PrintWriter(writer))
