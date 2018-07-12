b4b7a0b0a376f2ad437ee2f681e302199d04714c
Joseph Ivie <josephivie@gmail.com>
2018-07-12 17:06:38 -0600

    New docs, typealiases for common patterns

--- observable-property/src/main/kotlin/lk/kotlin/observable/property/lifecycle/Lifecycle.kt
+++ observable-property/src/main/kotlin/lk/kotlin/observable/property/lifecycle/Lifecycle.kt
@@ -2,4 +2,8 @@ package lk.kotlin.observable.property.lifecycle
 
 import lk.kotlin.observable.property.ObservableProperty
 
+/**
+ * Represents the lifecycle of something, `true` meaning that the thing is active and `false`
+ * meaning that the thing is inactive.
+ */
 typealias Lifecycle = ObservableProperty<Boolean>
